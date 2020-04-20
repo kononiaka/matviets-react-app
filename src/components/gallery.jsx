a809d94ad37bf27d23ac1cee864f0d4995211d69
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-04-20 21:02:54 +0300

    finalized

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Gallery = (props) => {
-  const galleryItems = props.elements.map((element) => (
-    <div className="galleryItem mt-4" key={element.id}>
+const Gallery = props => {
+  const galleryItems = props.elements.map(element => (
+    <div className="galleryItem" key={element.id}>
       <img src={element.src} />
     </div>
   ));
