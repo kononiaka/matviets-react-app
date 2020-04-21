2cd97a380f3b6b412d54f41d4a6ca397d5df9fcf
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-04-21 20:34:30 +0300

    link refs were added

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,48 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import Works from "./Works";
+import { Link } from "react-router-dom";
 
-class Menu extends Component {
-  state = {};
-  render() {
-    return (
-      <ul className="nav flex-column">
-        <li className="nav-item mb-2">
-          <a className="nav-link active " href="#" alt="1">
-            Home
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#" alt="1">
-            Works
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#" alt="1">
-            About
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#" alt="1">
-            Exhibition
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#" alt="1">
-            Store
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#" alt="1">
-            Contact
-          </a>
-        </li>
-        <li className="nav-item mb-2">
-          <a className="nav-link" href="#">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </li>
-      </ul>
-    );
-  }
-}
+const Menu = () => {
+  return (
+    <div className="nav flex-column">
+      <Link className="nav-link active " href="#" alt="1">
+        Home
+      </Link>
+
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#" alt="1">
+          Works
+        </Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#" alt="1">
+          About
+        </Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#" alt="1">
+          Exhibition
+        </Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#" alt="1">
+          Store
+        </Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#" alt="1">
+          Contact
+        </Link>
+      </li>
+      <li className="nav-item mb-2">
+        <Link className="nav-link" href="#">
+          <i class="fab fa-instagram"></i>
+        </Link>
+      </li>
+    </div>
+  );
+};
 
 export default Menu;
