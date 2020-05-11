eb57745aceecf90c45616457f44edd3ac7d2e037
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-05-11 17:31:46 +0300

    rebuild app

--- src/components/store/Store.jsx
+++ src/components/store/Store.jsx
@@ -1,47 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Store.css";
+import Header from "./../header/Header";
+import Links from "./../links/Links";
 
-const Store = () => {
+const Exhibition = () => {
   return (
     <>
-      <div className="container-custom">
-        <h4 className="header">MATVIETS VLADIMIR</h4>
-
+      <div className="container">
+        <Header></Header>
         <div className="row">
-          <div className="nav flex-column сol-5">
-            <Link className="nav-link-custom  " to="/" href="#" alt="1">
-              Home
-            </Link>
-
-            <Link className="nav-link-custom" to="/works" href="#" alt="1">
-              Works
-            </Link>
-
-            <Link className="nav-link-custom " to="/about" href="#" alt="1">
-              About
-            </Link>
-
-            <Link className="nav-link-custom" to="/exhibition" href="#" alt="1">
-              Exhibition
-            </Link>
-
-            <Link
-              className="nav-link-custom active"
-              to="/store"
-              href="#"
-              alt="1">
-              Store
-            </Link>
-            <Link className="nav-link-custom" to="/contact" href="#" alt="1">
-              Contact
-            </Link>
-            <a
-              className="nav-link-custom"
-              href="https://www.instagram.com/matviets.vladimir.artist/">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </div>
+          <Links></Links>
+          <div className="col-2"></div>
           <div className="col">
             <h6 className="works-text">
               Sorry. This content is currently unavailable
@@ -53,4 +21,4 @@ const Store = () => {
   );
 };
 
-export default Store;
+export default Exhibition;
